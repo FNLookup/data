d6683241e05663c595ed5dca01aa8a4d1d58767e
tposejank <81495861+tposejank@users.noreply.github.com>
2024-04-21 16:37:03 -0400

    Advanced blog posts

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
 # data
 FNLookup's Data backend.
 
-This is not yet worked on
-
-## fortnite-blog
-
-Todo: localize every language to its own folder, and every language saves 5 pages (13 items each) max totaling to 5 jsons per language
-
-if possible save all types of news (Juno, Sparks, BR, Delmar, Competitive) etc while doing all the localization and page saving
-
 ## fnlookup-blog
 
 MD files have blog content. Languages planned: 2 (en, es)
--- posts.py
+++ posts.py
@@ -1,9 +1,59 @@
+import os
 import requests
 import json
 import time
 
-def fetch_fortnite_posts():
-    url = 'https://www.fortnite.com/api/blog/getPosts?category=&postsPerPage=0&offset=0&locale=en-US&rootPageSlug=blog'
+language_list = ['en-US', 'ar', 'de', 'es-ES', 'es-MX', 'fr', 'it', 'ja', 'ko', 'pl', 'pt-BR', 'ru', 'tr']
+                # All
+categories_list = ['', 'battle-royale', 'creative', 'fortnite-festival', 'lego-fortnite', 'ranked', 'rocket-racing', 'save-the-world', 'team-rumble']
+
+fetch_list = [
+    {
+        "url": "https://www.fortnite.com/api/blog/getPosts?postsPerPage=0&offset=0&rootPageSlug=blog", # Adds language (&locale=) after and category after (&category=)
+        "path": "blog_posts_category_lang_page_0.json" # Replace lang with language and category with category
+    },{
+        "url": "https://www.fortnite.com/api/blog/getPosts?postsPerPage=13&offset=13&rootPageSlug=blog",
+        "path": "blog_posts_category_lang_page_1.json"
+    },{
+        "url": "https://www.fortnite.com/api/blog/getPosts?postsPerPage=13&offset=26&rootPageSlug=blog",
+        "path": "blog_posts_category_lang_page_2.json"
+    },{
+        "url": "https://www.fortnite.com/api/blog/getPosts?postsPerPage=13&offset=39&rootPageSlug=blog",
+        "path": "blog_posts_category_lang_page_3.json"
+    }
+]
+
+timestamps = {}
+
+markdown_string = '# Index - Blog Posts\n'
+
+def format_category(script):
+    words = script.split("-")
+    formatted_words = [word.capitalize() for word in words]
+    formatted_script = " ".join(formatted_words)
+    
+    return formatted_script
+
+def get_page(input_string):
+    digits = ""
+    
+    for char in input_string:
+        if char.isdigit():
+            digits += char
+    
+    try:
+        result = int(digits)
+    except ValueError:
+        result = None
+    
+    return result
+
+directory = "posts"
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+def fetch_fortnite_posts(url):
+    # url = 'https://www.fortnite.com/api/blog/getPosts?category=&postsPerPage=0&offset=0&locale=en-US&rootPageSlug=blog'
     headers = {
         'Cache-Control': 'no-cache',
         'Accept': 'application/json, text/plain, */*',
@@ -11,12 +61,10 @@ def fetch_fortnite_posts():
         'Referer': 'https://www.fortnite.com/news',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
     }
-
-    print("Attempting to fetch", url)
     
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        print("Fetch successful")
+        print("Success:", response.status_code)
         return response.json()
     else:
         print(f"Failed to fetch Fortnite API: {response.status_code}")
@@ -24,11 +72,40 @@ def fetch_fortnite_posts():
         return None
 
 if __name__ == "__main__":
-    fortnite_posts = fetch_fortnite_posts()
-    if fortnite_posts:
-        with open("posts/blog_posts.json", "w") as file:
-            json.dump(fortnite_posts, file, indent=4)
-            print("Data saved to file")
-        with open("posts/timestamp.json", "w") as file:
-            file.write('{"timestamp": ' + str(time.time()) + '}')
-            print("Timestamp saved to file")
+    for language in language_list:
+
+        markdown_string += f'\n## {language}\n|Entry|File|\n|-|-|\n'
+
+        for category in categories_list:
+
+            path_category = category
+
+            if path_category == '':
+                path_category = 'all'
+
+            for fetch in fetch_list:
+
+                url = fetch['url'] + f'&locale={language}&category={category}'
+                print("GET:", url)
+                print("Language:", language)
+                print("Category:", path_category)
+
+                fortnite_posts = fetch_fortnite_posts(url)
+                if fortnite_posts:
+                    path = fetch["path"].replace('category', path_category).replace('lang', language)
+                    with open("posts/" + path, "w") as file:
+                        json.dump(fortnite_posts, file, indent=4)
+                        print(f'Data saved to file: posts/{path}')
+                    timestamps[path.replace('.json', '')] = time.time()
+
+                markdown_string += f'|{format_category(path_category)} #{get_page(path) + 1}|[{path}](https://github.com/FNLookup/data/blob/main/posts/{path})|\n'
+
+
+    with open("posts/timestamps.json", "w") as file:
+        json.dump(timestamps, file, indent=4)
+
+        print("Timestamps saved to file")
+
+    with open("posts/README.md", "w") as file:
+        file.write(markdown_string)
+        print("Markdown saved to file")
